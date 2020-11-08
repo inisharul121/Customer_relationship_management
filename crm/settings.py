103fcf5235c78e9e3494e3b71fe8357cddd7ac60
inisharul121 <inisharul121@gmail.com>
2020-11-08 19:07:21 +0600

    setup.py  uploaded

--- crm/settings.py
+++ crm/settings.py
@@ -129,7 +129,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_URL='/static/'
+
 STATIC_MEDIA = '/images/'
+
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS=[
